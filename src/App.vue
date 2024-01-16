a62a0096c0b72981b350a6fe72b38f5c3d98eace
Ivan Radojevic <ivan.radojevic@kraftwerk.co.at>
2024-01-16 20:57:47 +0100

    added font family

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="flex flex-wrap space-x-2">
-    <header class="bg-red-500 flex flex-wrap space-x-2">
-      <h1 class="flex flex-wrap space-x-2"><span>Ivan</span><span>Radojevic</span></h1>
+  <div class="font-essential text-essential space-essential">
+    <header class="bg-red-500 flex flex-wrap space-essential">
+      <h1 class="flex flex-wrap space-essential"><span>Ivan</span><span>Radojevic</span></h1>
 
       <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
       <HelloWorld msg="You did it!" /> -->
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        essential: ['Arimo', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
