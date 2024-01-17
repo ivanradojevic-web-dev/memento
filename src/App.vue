8bb1f53ede89199326496069f82124634e4e8691
Ivan Radojevic <ivan.radojevic@kraftwerk.co.at>
2024-01-17 14:45:16 +0100

    templating home view

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,21 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="text-essential space-essential font-essential">
-    <header class="space-essential flex flex-wrap bg-golden-yellow text-davys-grey">
+  <div class="text-essential font-essential">
+    <!-- <header class="space-essential flex flex-wrap bg-golden-yellow text-davys-grey">
       <h1 class="space-essential flex flex-wrap"><span>Ivan</span><span>Radojevic</span></h1>
-
-      <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-      <HelloWorld msg="You did it!" /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </header>
+    </header> -->
 
     <RouterView />
+
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+     <HelloWorld msg="You did it!" /> -->
+
+    <!-- <footer>Foo</footer> -->
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <header class="space-essential bg-golden-yellow text-davys-grey">
+    <h1 class="space-essential"><span>Ivan</span><span>Radojevic</span></h1>
+
+    <nav class="space-essential">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/contact">Contact</RouterLink>
+    </nav>
+  </header>
 </template>
